2f690cdfb9c97b4a24df30b89c50646eb7bf8f98
EA - Gabriel Quintanilha Cabral De Oliveira <gabriel_quintanilha@educadventista.org>
2023-08-29 20:30:57 -0300

    iniciando a nova atividade

--- 2_EXPRESS/7_adicionando_css/index.js
+++ 2_EXPRESS/7_adicionando_css/index.js
@@ -10,38 +10,4 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
-//utilizar arquivos estáticos
-app.use(express.static('public'))
-
-app.post('/users/save', (req, res) => {
-    const nome = req.body.nome
-    const idade = req.body.idade
-
-    console.log(`
-        Usuário: ${nome}
-        Idade: ${idade}
-    `)
-
-    res.redirect('/')
-})
-
-app.get('/users/cadastrar', (req, res) => {
-    res.sendFile(`${caminho}/usuariosform.html`)
-})
-
-app.post('/users/save', (req, res) => {
-
-}) 
-
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id
-
-    console.log(`Estamos buscando pelo usuário: ${id}`)
-    res.sendFile(`${caminho}/usuarios.html`)
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(`${caminho}/index.html`)
-})
-
-app.listen(8080)
+app.listen(5000)
---
+++ 2_EXPRESS/8_atividade/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const caminho = path.join(__dirname, 'templates')
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//utilizar arquivos estáticos
+app.use(express.static('public'))
+
+app.post('/users/save', (req, res) => {
+    const nome = req.body.nome
+    const idade = req.body.idade
+
+    console.log(`
+        Usuário: ${nome}
+        Idade: ${idade}
+    `)
+
+    res.redirect('/')
+})
+
+app.get('/users/cadastrar', (req, res) => {
+    res.sendFile(`${caminho}/usuariosform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+
+}) 
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+    res.sendFile(`${caminho}/usuarios.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${caminho}/index.html`)
+})
+
+app.listen(8080)

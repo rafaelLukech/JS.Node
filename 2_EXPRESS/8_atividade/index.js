7166c49c07a2a9891660d755a4378e8deccdd113
Quintanilha09 <gabriel.hemendinger@gmail.com>
2023-09-04 00:16:33 -0300

    desenvolvendo a atividade e os mapeamentos

--- 2_EXPRESS/8_atividade/index.js
+++ 2_EXPRESS/8_atividade/index.js
@@ -4,16 +4,21 @@ const app = express()
 const path = require('path')
 const caminho = path.join(__dirname, 'templates')
 
+app.use(express.static('public'))
+
 app.use(express.urlencoded({
     extended: true
 }))
 
-app.use(express.json())
+app.get('/', (req, res) => {
+    res.sendFile(`${caminho}/index.html`)
+})
 
-//utilizar arquivos estáticos
-app.use(express.static('public'))
+app.get('/usuario/cadastro', (req, res) => {
+    res.sendFile(path.join(caminho, 'cadastro.html'))
+})
 
-app.post('/users/save', (req, res) => {
+app.post('/usuario/salvar', (req, res) => {
     const nome = req.body.nome
     const idade = req.body.idade
 
@@ -25,23 +30,6 @@ app.post('/users/save', (req, res) => {
     res.redirect('/')
 })
 
-app.get('/users/cadastrar', (req, res) => {
-    res.sendFile(`${caminho}/usuariosform.html`)
-})
+app.use(express.json())
 
-app.post('/users/save', (req, res) => {
-
-}) 
-
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id
-
-    console.log(`Estamos buscando pelo usuário: ${id}`)
-    res.sendFile(`${caminho}/usuarios.html`)
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(`${caminho}/index.html`)
-})
-
-app.listen(8080)
+app.listen(5000)
